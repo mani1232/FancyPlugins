e940a2c52624809e032316e21ce74378d5eaf6bf
mani1232 <support@worldmandia.cc>
2025-05-14 21:04:59 +0200

    Fix java version error

--- libraries/packets/implementations/build.gradle.kts
+++ libraries/packets/implementations/build.gradle.kts
@@ -1,3 +1,19 @@
 plugins {
     id("java-library")
 }
+
+allprojects {
+
+    apply(plugin = "java-library")
+
+    java {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+    }
+
+    tasks {
+        compileJava {
+            options.encoding = Charsets.UTF_8.name()
+            options.release = 21
+        }
+    }
+}
